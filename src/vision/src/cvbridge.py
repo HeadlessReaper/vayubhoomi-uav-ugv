d007e6c187197214cc38e14ab83c1773cf33b2a8
SR42-dev <sriram.radhakrishna42@gmail.com>
2022-07-14 10:45:11 +0530

    CVBridge up and running.

--- src/vision/src/cvbridge.py
+++ src/vision/src/cvbridge.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python
+'''
+import cv2
+
+while True :
+
+    img = cv2.imread('test-image.jpg')
+
+    cv2.imshow('Image', img)
+
+    if cv2.waitKey(0) == ord('q'):
+        break
+'''
 
 import rospy
 import cv2
@@ -10,35 +21,45 @@ import sys
 bridge = CvBridge()
 
 def image_callback(ros_image):
-  print('image')
+
+  print('Image obtained.')
   global bridge
-  #convert ros_image into an opencv-compatible image
+
+  # convert ros_image into an opencv-compatible image
+
   try:
     cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
   except CvBridgeError as e:
-      print(e)
-  #from now on, you can work exactly like with opencv
-  (rows,cols,channels) = cv_image.shape
-  if cols > 200 and rows > 200 :
-      cv2.circle(cv_image, (100,100),90, 255)
-  font = cv2.FONT_HERSHEY_SIMPLEX
-  cv2.putText(cv_image,'Webcam Activated with ROS & OpenCV!',(10,350), font, 1,(255,255,255),2,cv2.LINE_AA)
-  cv2.imshow("Image window", cv_image)
-  cv2.waitKey(3)
+    print(e)
+
+  # from this point on, cv_image is the target of processing
+  
+  cv2.imshow("Image", cv_image)
+
+  #cv2.waitKey(3)
+  if (cv2.waitKey(1) == ord('q')):
+        break
 
   
 def main(args):
-  rospy.init_node('image_converter', anonymous=True)
-  #for turtlebot3 waffle
-  #image_topic="/camera/rgb/image_raw/compressed"
-  #for usb cam
-  #image_topic="/usb_cam/image_raw"
-  image_sub = rospy.Subscriber("/usb_cam/image_raw",Image, image_callback)
+
+  rospy.init_node('vision', anonymous=True)
+
+  #`for turtlebot3 waffle
+  #`image_topic="/camera/rgb/image_raw/compressed"
+  #`for usb cam
+  #`image_topic = "/usb_cam/image_raw"
+  #`for hector quadrotor
+  image_topic = "/front_cam/camera/image"
+  image_sub = rospy.Subscriber(image_topic,Image, image_callback)
+
   try:
     rospy.spin()
   except KeyboardInterrupt:
     print("Shutting down")
+
   cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+
     main(sys.argv)
