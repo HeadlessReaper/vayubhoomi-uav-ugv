00bbfeb62097917e6d6b43fc8b74e8e08420a882
pramukmp <pramuk2103@gmail.com>
2022-08-04 00:55:08 +0530

    additional files no use

--- src/lidar/src/dronemove.py
+++ src/lidar/src/dronemove.py
@@ -16,9 +16,10 @@ integ=0
 x_p=0
 y_p=0
 z_p=0
-dist1=0
-dist2=0
-dist3=0
+dist1=[]
+dist2=[]
+dist3=[]
+dist4=[]
 
 def poseCallback(pose_message):
     global x_p
@@ -31,76 +32,91 @@ def poseCallback(pose_message):
     (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
     yaw=round(yaw,2)
     
-def distCallback(msg):
-    global dist1
-    global dist2 , dist3
-    dist1=round(msg.ranges[540],2)
-    dist2=round(msg.ranges[8],2)
-    dist3=round(msg.ranges[1070],2)#backwards
-    print(" Distance from scan topic : ",dist1,dist2,dist3)
+def distCallback(data):
+    global dist1,dist2,dist3,dist4
+
+    dist1=data.ranges[520:560]
+    dist2=data.ranges[:25:]+data.ranges[:-25:-1]
+    dist3=data.ranges[160:200]#right
+    dist4=data.ranges[880:920]#left
+    #print(" Distance from scan topic : ",dist1,dist2,dist3)
 
 def move(xgoal,ygoal,zgoal):
     global x_p,y_p, z_p
     global dist1,dist2,dist3,distp,yaw,errorp,integ
-    vmsg = Twist()
-    pub=rospy.Publisher('/quadrotor/cmd_vel',Twist,queue_size=10)  
 
-    vmsg.linear.z=0.8
+    count_r = 0
+    count_l = 0
+    count_f = 0
+
+    vmsg = Twist()
+    pub=rospy.Publisher('/cmd_vel',Twist,queue_size=10)  
+
+    vmsg.linear.z=1
     pub.publish(vmsg)
     time.sleep(0.8)
 
     while(True):
-        distance = round(abs(math.sqrt(((xgoal-x_p) ** 2) + ((ygoal-y_p) ** 2)+((zgoal-z_p) ** 2))),2)
-        print(distance)
-        error=distance-distp
+
+        # distance = round(abs(math.sqrt(((xgoal-x_p) ** 2) + ((ygoal-y_p) ** 2)+((zgoal-z_p) ** 2))),2)
+        # print(distance)
+        # error=distance-distp
         der = error - errorp
         integ= integ+error
 
-        kp=2
-        ki=0
-        kd=0
+        kp=2.5
+        ki=0.0003
+        kd=0.4
           
-        linear_speed = kp*distance+ki*integ+(kd*der)
+        linear_speed = kp*error+ki*integ+(kd*der)
+
         print('error',error,'Integ : ',ki*integ, 'linear speed',linear_speed)
 
 
-        K_angular = 4.0
-        desired_angle_goal = math.atan2(ygoal-y_p, xgoal-x_p)
-        angular_speed = (desired_angle_goal-yaw)*K_angular
+        # K_angular = 4.0
+        # desired_angle_goal = math.atan2(ygoal-y_p, xgoal-x_p)
+        # angular_speed = (desired_angle_goal-yaw)*K_angular
 
-        vmsg.linear.x = linear_speed
-        vmsg.angular.z = angular_speed
+        vmsg.linear.x = 0.1
+        #vmsg.angular.z = angular_speed
 
-        if((dist1>0.5 or dist1==inf)):
-            pub.publish(vmsg)
-            print ('x=', x_p, 'y=',y_p)
-            distp=distance
-            errorp=error
-        elif ((x_p==xgoal and y_p==ygoal and z_p==zgoal) or distance<0.5):            
-            vmsg.angular.z = 0
-            print('*********************************************************')
-            pub.publish(vmsg)
-            break
-        if(dist1<0.5 ):#or dist2<1.5 or dist3<1.5
-            print('********************obstacle seen*****************')
-            print('dist :',dist1)
-            vmsg.linear.x = -1
-            pub.publish(vmsg)
-            time.sleep(1)
-            vmsg.linear.x = 0
-            vmsg.linear.z=0
-            pub.publish(vmsg)
-            break
+        if((next((True for elem in dist3 if elem <2), False)==False)): 
+            count_r+=1
+            print(len(dist3))
+            i=dist3[20]
+            error = 1/i
+            vmsg.linear.y = ((kp*error) + (ki*integ) + (kd*der))
+            vmsg.linear.z = 0
+        elif((next((True for elem in dist4 if elem <2), False)==False)):  
+            count_l+=1
+            i=dist4[20]
+            error = 1/i
+            vmsg.linear.y = -((kp*error)+(ki*integ)+(kd*der))
+            vmsg.linear.z = 0
+        elif((next((True for elem in dist1 if elem <2), False)==False)):
+            print('*********************************')
+            count_f+=1
+            i=dist2[25]
+            error = 1/i
+            vmsg.linear.z = 1
+        # elif ((x_p==xgoal and y_p==ygoal and z_p==zgoal) ):            
+        #     vmsg.angular.z = 0
+        #     print('*********************************************************')
+        #     pub.publish(vmsg)
+        #     break
+        pub.publish(vmsg)
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('drone',anonymous=False)
-        velpub=rospy.Publisher('/quadrotor/cmd_vel',Twist,queue_size=10)
+        velpub=rospy.Publisher('/cmd_vel',Twist,queue_size=10)
         possub=rospy.Subscriber('ground_truth/state',Odometry,poseCallback)
-        distsub=rospy.Subscriber('/quadrotor/scan',LaserScan,distCallback)
+        distsub=rospy.Subscriber('/scan',LaserScan,distCallback)
         time.sleep(0.5)
-        move(4,-5,10)
+        l=[(1,1,2),(1,6,2),(6,6,2),(6,1,2)]
+        for i in l:
+            move(i[0],i[1],i[2])
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated.")
--- src/lidar/src/obsavoidance.py
+++ src/lidar/src/obsavoidance.py
@@ -2,6 +2,7 @@
 from cmath import inf
 import math
 import rospy
+from lidar.msg import distlist
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
@@ -11,6 +12,31 @@ import time
 integ=0
 distp=0
 errorp=0
+dist3=0
+dist4=0
+dist1=[]
+dist2=[]
+dist5=[]
+dist6=[]
+
+def poseCallback(pose_message):
+    global x
+    global y, yaw
+    orientation_q = pose_message.pose.pose.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+    yaw=round(yaw,2)
+    x= round(pose_message.pose.pose.position.x,2)
+    y= round(pose_message.pose.pose.position.y,2)
+
+def distCallback(msg):
+    global dist1,dist2,dist3,dist4,dist5,dist6
+    dist1=msg.ranges[8:100]#left 0 to 75
+    dist2=msg.ranges[200:350]#right
+    dist5=msg.ranges[100:270]
+    dist6=msg.ranges[:45:]+msg.ranges[:-45:-1]
+    dist3=msg.ranges[10] 
+    dist4=msg.ranges[310]
 
 def check(d1,d):
     for elem in d1:
@@ -18,11 +44,11 @@ def check(d1,d):
             print('heeeeeeeeellllllllllllllllllllllllllllllloooooooo')
             return True
         else:
-            print('Nooooooooooooooooooooo')
-            return False
+            pass
 
-def rotate (angular_speed_degree, relative_angle_degree, clockwise,dist6,dist5,yaw):
+def rotate (angular_speed_degree, relative_angle_degree, clockwise):
 
+    global dist5,dist6,yaw
     vmsg = Twist()
     vmsg.linear.x=0
     vmsg.linear.y=0
@@ -45,7 +71,7 @@ def rotate (angular_speed_degree, relative_angle_degree, clockwise,dist6,dist5,y
 
     t0 = rospy.Time.now().to_sec()
 
-    while (check(dist5,0.25)==False) :
+    while ((next((True for elem in dist5 if elem <0.2), False)==False )and (next((True for elem in dist6 if elem <0.2), False)==False )) :
         print("Turtlesim rotates")
         velocity_publisher.publish(vmsg)
 
@@ -58,32 +84,37 @@ def rotate (angular_speed_degree, relative_angle_degree, clockwise,dist6,dist5,y
         if  (current_angle_degree>relative_angle_degree):
             print('reached')
             break
+    else:
+        if(check(dist6,0.2)==True):
+            print('rotate fn reverse')
+            vmsg.linear.x=-0.1
+            velocity_publisher.publish(vmsg)
+            time.sleep(0.4)
+        elif(check(dist5,0.2)==True):
+            print('rotate fn forward')
+            vmsg.linear.x=0.1
+            velocity_publisher.publish(vmsg)
+            time.sleep(0.4)
 
-    if(check(dist6,0.2)==True):
-        print('rotate fn reverse')
-        vmsg.linear.x=-0.1
-        velocity_publisher.publish(vmsg)
-        time.sleep(0.4)
-    elif(check(dist5,0.25)==True):
-        print('rotate fn forward')
-        vmsg.linear.x=0.1
-        velocity_publisher.publish(vmsg)
-        time.sleep(0.4)
-
-def obscheck(dist1,dist2,dist3,dist5,dist6,x,y,yaw):
+def obscheck():
     
-    global integ,errorp
+    global dist1,dist2,dist3,dist4,dist5,dist6,integ,errorp
+
+    #rospy.init_node('obsnodes',anonymous=False)
+    veltop='/cmd_vel'
+    pub=rospy.Publisher(veltop,Twist,queue_size=10)
+    postop='/scan'
+    possub=rospy.Subscriber(postop,LaserScan,distCallback)
+    dissub=rospy.Subscriber('/odom',Odometry,poseCallback)
 
     velocity_message = Twist()
-    cmd_vel_topic='/cmd_vel'
-    pub=rospy.Publisher(cmd_vel_topic,Twist,queue_size=10)   
-         
+
     #rotate(15,60,False)
-    kp =0.2
+    kp =0.1
     ki=0
     kd=0
 
-    error=1/dist3
+    error= 0 if dist3==inf else 1/dist3
     der = error - errorp
     integ= 0.67*integ+error
     
@@ -97,11 +128,11 @@ def obscheck(dist1,dist2,dist3,dist5,dist6,x,y,yaw):
     velocity_message.linear.x = 0.707*linear_speed +0.04
     #velocity_message.angular.z = angular_speed
 
-    if(next((True for elem in dist1 if elem <0.5), False)):
+    if(next((True for elem in dist1 if elem <0.4), False)):
         print('******************** obstacle on left ********************')
         #print('dist1 :',dist1,'dist2 :',dist2,'dist3 :' ,dist3)
         print("linear speed",linear_speed)
-        if (next((True for elem in dist1 if elem <0.25), False)):
+        if (next((True for elem in dist1 if elem <0.2), False)):
             velocity_message.linear.x=-0.65
             pub.publish(velocity_message) 
             time.sleep(0.5)
@@ -115,11 +146,11 @@ def obscheck(dist1,dist2,dist3,dist5,dist6,x,y,yaw):
             time.sleep(0.5)
             return False
 
-    elif(next((True for elem in dist2 if elem <0.5), False)):
+    elif(next((True for elem in dist2 if elem <0.4), False)):
         print('******************** obstacle on right *******************')
         #print('dist :',min(dist1,dist2,dist3))
         print("linear speed",linear_speed)
-        if (next((True for elem in dist2 if elem <0.25), False)):
+        if (next((True for elem in dist2 if elem <0.2), False)):
             velocity_message.linear.x=-0.65
             pub.publish(velocity_message)
             time.sleep(0.5)
@@ -137,13 +168,8 @@ def obscheck(dist1,dist2,dist3,dist5,dist6,x,y,yaw):
         
 if __name__ == '__main__':
     try:
-        rospy.init_node('obsnode',anonymous=True)
-        veltop='/cmd_vel'
-        velpub=rospy.Publisher(veltop,Twist,queue_size=10)
-        # postop='/odom'
-        # possub=rospy.Subscriber(postop,Odometry,poseCallback)
+
         # disttop='/scan'
-        # distsub=rospy.Subscriber(disttop,LaserScan,distCallback)
         time.sleep(1)
         #move(0.2,0.35,True)
         
