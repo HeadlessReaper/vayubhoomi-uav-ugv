0024fa27b41065948c0cf2ccce479d6f71eefb80
pramukmp <pramuk2103@gmail.com>
2022-08-03 12:40:06 +0530

    fixing bugs

--- src/lidar/src/droneobsavoid.py
+++ src/lidar/src/droneobsavoid.py
@@ -17,8 +17,8 @@ def callback(data):
 	global dist1, dist2, dist3,dist4
 
 	dist1=data.ranges[500:560] #forward
-	dist4=data.ranges[120:220]#left
-	dist3=data.ranges[850:950]#right
+	dist4=data.ranges[150:320]#left
+	dist3=data.ranges[800:950]#right
 
 	error_p = 0
 	error = 0
@@ -37,7 +37,7 @@ def callback(data):
 	count_l = 0
 	count_f = 0
 
-	kp = 2.5
+	kp = 2.8
 	ki = 0.0003
 	kd = 0.4
 
@@ -50,20 +50,20 @@ def callback(data):
 
 
 	# Obs on three sides
-	if ((next((True for elem in dist3 if elem <2), False)==True) and (next((True for elem in dist1 if elem <2), False)==True and (next((True for elem in dist4 if elem <2), False)==True))):
-		print('ons on three sides')
-		error=3/(dist1[25]+dist3[25]+dist4[25])
-		msg.linear_x = -((kp * error) + (ki * integ) + (kd * der))
-		pub.publish(msg)
+	# if ((next((True for elem in dist3 if elem <2), False)==True) and (next((True for elem in dist1 if elem <2), False)==True and (next((True for elem in dist4 if elem <2), False)==True))):
+	# 	print('ons on three sides')
+	# 	error=3/(dist1[25]+dist3[25]+dist4[25])
+	# 	msg.linear_x = -((kp * error) + (ki * integ) + (kd * der))
+	# 	pub.publish(msg)
 		
-	# Obs on both sides
-	elif ((next((True for elem in dist3 if elem <2), False)==True) and (next((True for elem in dist4 if elem <2), False)==True)):
-		print('obs on 2 sides')
-		error=2/(dist3[25]+dist4[25])
-		msg.linear_x = (kp * error) + (ki * integ) + (kd * der)
-		pub.publish(msg)
+	# # Obs on both sides
+	# elif ((next((True for elem in dist3 if elem <2), False)==True) and (next((True for elem in dist4 if elem <2), False)==True)):
+	# 	print('obs on 2 sides')
+	# 	error=2/(dist3[25]+dist4[25])
+	# 	msg.linear_x = (kp * error) + (ki * integ) + (kd * der)
+	# 	pub.publish(msg)
 	
-	elif((next((True for elem in dist3 if elem <2), False)==True)):
+	if((next((True for elem in dist3 if elem <2), False)==True)):
 		print('right') 
 		#print(len(dist3))
 		i=dist3[20]
